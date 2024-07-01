d2c6051a2a87e0e0a84f3f77cd4dfe8e8a8ce9fe
Christoph Grabo <asaaki@mannaz.cc>
2024-07-01 13:06:13 +0200

    Fix bench setup for CI

--- benches.sh
+++ benches.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # export RUST_BACKTRACE=1
 export RUSTFLAGS="-Ctarget-cpu=native"
-cargo bench -q --all-features --bench brlv
-cargo bench -q --all-features --bench brlu
+cargo bench -q --all-features
