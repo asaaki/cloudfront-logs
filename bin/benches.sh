47beb9c73177206cc92f55d083b74b7fb0082ce7
Christoph Grabo <asaaki@mannaz.cc>
2024-07-01 13:19:12 +0200

    Add bin folder and move utilities

--- bin/benches.sh
+++ bin/benches.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd $(git rev-parse --show-toplevel)
+
 # export RUST_BACKTRACE=1
 export RUSTFLAGS="-Ctarget-cpu=native"
 cargo bench -q --all-features
--- bin/docs.sh
+++ bin/docs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+cd $(git rev-parse --show-toplevel)
+
 RUSTDOCFLAGS="--cfg docsrs" \
     cargo +nightly doc \
     --all-features --no-deps
