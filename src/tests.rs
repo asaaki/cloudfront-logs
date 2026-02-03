628f7645291035e1edd518cea7c98958565c8e96
Christoph Grabo <asaaki@mannaz.cc>
2026-02-03 13:18:22 +0100

    Update Cargo dependencies to latest version

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,6 @@
 use crate::*;
 use ::parquet::{record::RecordWriter, schema::parser::parse_message_type};
 use std::net::{Ipv4Addr, Ipv6Addr};
-use time::macros::{date, time};
 
 const AWS_DOCS_EXAMPLE: &str = r#"#Version: 1.0
 #Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken x-forwarded-for ssl-protocol ssl-cipher x-edge-response-result-type cs-protocol-version fle-status fle-encrypted-fields c-port time-to-first-byte x-edge-detailed-result-type sc-content-type sc-content-len sc-range-start sc-range-end
